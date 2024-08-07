44f1d15058e4aaf29a5e5b131958cf5a04da93ff
Владислав Марук <flatydoc@MacBook-Pro-Vladislav.local>
2024-08-07 10:44:23 +0300

    commit

--- src/core/services/index.ts
+++ src/core/services/index.ts
@@ -1,18 +1,18 @@
-import axios from 'axios';
-import WebApp from '@twa-dev/sdk';
-import { getTokenByTgInitData } from './login.service';
+import axios from "axios";
+import WebApp from "@twa-dev/sdk";
+import { getTokenByTgInitData } from "./login.service";
 
 export const api = axios.create({
-  baseURL: `https://happyfarmclicker.shop/api/v1`,
+  baseURL: `https://happyfarmclicker.shop/api/v1/test`,
   //baseURL: `http://localhost:5001/api/v1`,
-  withCredentials: true,
+  //withCredentials: true,
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token !== null) {
     const data = JSON.parse(token);
     config.headers.Authorization = `Bearer ${data.token}`;
@@ -37,10 +37,10 @@ api.interceptors.response.use(
       try {
         const data = WebApp.initData;
         const { token } = await getTokenByTgInitData(data);
-        localStorage.setItem('token', JSON.stringify(token));
+        localStorage.setItem("token", JSON.stringify(token));
         return api.request(originalRequest);
       } catch (e) {
-        console.log(e, 'UnauthorizedError');
+        console.log(e, "UnauthorizedError");
       }
     }
     throw error;
